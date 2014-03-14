777f96663fd98ec0e08407e233bc7db399e2e204
Robert Kuykendall <robert@rkuykendall.com>
2014-03-14 02:44:02 -0400

    Heroku fixes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Fannetic::Application.configure do
+  config.assets.initialize_on_precompile = false
+
+
   config.action_mailer.default_url_options = { :host => 'fannetic.com' }
 
 
@@ -30,7 +33,7 @@ Fannetic::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = false  
 
   # Generate digests for assets URLs.
   config.assets.digest = true
